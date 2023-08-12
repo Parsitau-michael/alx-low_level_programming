1ece49dbd00124ff8e8dfbfe7bb39999c8e2bc6b
Parsitau-michael <parsitaumichael@gmail.com>
2023-08-12 03:13:13 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
-/*we start with including the standard io*/
+
 #include<stdio.h>
-/* This is the main function */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	/* the function to display our code */
 	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
