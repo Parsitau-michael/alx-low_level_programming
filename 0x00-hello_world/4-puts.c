82600a319e025853750776a26ae04db4a518e2b1
Parsitau-michael <parsitaumichael@gmail.com>
2023-08-11 19:15:59 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,8 @@
-#!/bin/bash
-put("Programming is like building a multilingual puzzle,"\n)
+
+#include<stdio.h>
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle,");
+		return(0);
+}
