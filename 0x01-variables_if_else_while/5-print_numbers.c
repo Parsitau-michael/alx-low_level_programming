05ed111448bc34688c0dee9ed7571902acc5d469
Parsitau-michael <parsitaumichael@gmail.com>
2023-08-13 16:18:14 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+	putchar('\n');
+	return (0);
+}
