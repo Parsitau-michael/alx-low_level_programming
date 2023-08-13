afada9f2616843f7fc3aeba663c550af70207486
Parsitau-michael <parsitaumichael@gmail.com>
2023-08-13 15:31:39 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	char c;
+	char C;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar (c);
+	}
+	for (C = 'A'; C <= 'Z'; C++)
+	{
+		putchar (C);
+	}
+	putchar('\n');
+	return (0);
+}
