a7ba6719dad01d962eb8667869be6730802b7d28
Parsitau-michael <parsitaumichael@gmail.com>
2023-08-14 12:04:15 -0700

    last task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i; j < 100; j++)
+		{
+			putchar('0' + (i / 10));
+			putchar('0' + (i % 10));
+			putchar(' ');
+			putchar('0' + (j / 10));
+			putchar('0' + (j % 10));
+
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
