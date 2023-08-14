e63921557fa106292b5ecd8fb209b13f4426faf4
Parsitau-michael <parsitaumichael@gmail.com>
2023-08-14 06:11:14 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 10; d++)
+	{
+		putchar('0' + d);
+
+		if (d != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
