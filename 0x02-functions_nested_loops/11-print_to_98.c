344f1a9bdc6d6cf73de57d3bccaaf121c8ba5434
Parsitau-michael <parsitaumichael@gmail.com>
2023-08-16 07:00:47 -0700

    printed natural numbers from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - a function that prints all natural numbers from n to 98
+ *
+ * @n: is the starting point from which we begin printing
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
