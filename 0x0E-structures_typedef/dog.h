f7fe941d98348526f4c33be7fbd994ce284be46e
Parsitau-michael <parsitaumichael@gmail.com>
2023-09-12 09:47:39 -0700

    Defined a new type dog_t as a new name for the type struct dog.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,6 +18,11 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - a typedef for struct dog.
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
