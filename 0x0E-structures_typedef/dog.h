f16bde4d1ff7fc87c17d6b13d7ebb58503c22c28
Parsitau-michael <parsitaumichael@gmail.com>
2023-09-12 05:06:36 -0700

    created a struct for the dogs info

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a stuct that takes the dogs info
+ *
+ * @name:the name of the dog
+ * @age: the age of the dog
+ * @owner:the owner of the dog
+ *
+ * Description: created a struct that takes dogs info
+ */
+
 struct dog {
 	char *name;
 	float age;
