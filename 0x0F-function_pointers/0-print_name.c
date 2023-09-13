b53f36d2a56450d0f95f739a6ccf90465d700490
Parsitau-michael <parsitaumichael@gmail.com>
2023-09-13 11:35:33 -0700

    corrected two errors, caused by not including the case of both f and name being null

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * print_name - a function that prints a name.
@@ -10,5 +11,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
