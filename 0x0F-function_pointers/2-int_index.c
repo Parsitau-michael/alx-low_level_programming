4d0c67b180fde1a68f96fb740c815c26f419b9d4
Parsitau-michael <parsitaumichael@gmail.com>
2023-09-13 14:22:31 -0700

    Included the case of the array and cmp being NULL.

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * int_index - a function that searches for an integer.
@@ -16,7 +17,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
