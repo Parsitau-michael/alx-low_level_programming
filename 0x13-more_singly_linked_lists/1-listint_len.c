b3c43a350d16e7c9624e3cc3c649fa456388ac94
Parsitau-michael <parsitaumichael@gmail.com>
2023-09-25 15:40:30 -0700

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - a function that returns the number of elements
+ * in a linked listint_t list.
+ *
+ * @h: a pointer to listint_t.
+ * Return: the number of elements in a linked listint_t list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
