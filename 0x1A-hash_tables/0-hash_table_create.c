db7f2e72eafd4c65aa3432db029c919c40d2e2c1
Parsitau-michael <parsitaumichael@gmail.com>
2024-04-13 14:49:39 -0700

    Corrected some betty errors

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,7 +3,7 @@
 /**
  * hash_table_create - a function that creates a hash table.
  *
- * size: is the size of the array.
+ * @size: is the size of the array.
  * Return: a pointer to the newly created hash table, otherwise NULL.
  */
 hash_table_t *hash_table_create(unsigned long int size)
