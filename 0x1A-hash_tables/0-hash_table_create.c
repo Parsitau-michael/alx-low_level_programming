a28e4d3c911b9f55e0030568de80c24afe712228
Parsitau-michael <parsitaumichael@gmail.com>
2024-04-14 03:03:41 -0700

    Corrected the part of memory allocation for the array to hash_node_t *

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,13 +8,25 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	hash_table_t *table;
+	unsigned long int i;
 
-	if (new_table == NULL)
+	/* Allocating memory for the table */
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
 		return (NULL);
 
-	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_table_t) * size);
+	/* Alloctaing memory for the aarray */
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
 
-	return (new_table);
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+	table->size = size;
+
+	return (table);
 }
