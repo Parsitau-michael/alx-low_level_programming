0c97e041bebae00af324de38930fb91e49a62af2
Parsitau-michael <parsitaumichael@gmail.com>
2024-07-17 13:52:54 -0700

    Updated my search_algos file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,5 +9,7 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int binary_helper(int *array, size_t low, size_t high, int value);
 
 #endif /* SEARCH_ALGOS_H */
